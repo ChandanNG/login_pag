841b873ea657fd80d296e05cffba038bd5f86f20
ChandanNG <126440225+ChandanNG@users.noreply.github.com>
2023-05-05 11:06:32 +0530

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-h032w#v=(izx(cr42#cp3#1jxu=l6xis!bcwtbu^7#-+*imong
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+
 
 
 # Application definition
@@ -127,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR,"static"),
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
