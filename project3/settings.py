ea424e6b19f82417a640862e82528c1c6bf28615
ChandanNG <126440225+ChandanNG@users.noreply.github.com>
2023-05-05 11:31:16 +0530

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-h032w#v=(izx(cr42#cp3#1jxu=l6xis!bcwtbu^7#-+*imong
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app",'.now.sh']
 
 
 
